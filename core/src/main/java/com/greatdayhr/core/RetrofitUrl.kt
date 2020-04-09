17e4fdaa1272cecd96ec5227b42503c4eef76829
Hendi Hidayat <hendi.evolution@gmail.com>
2020-04-09 13:33:28 +0700

    Retrofit url with

--- core/src/main/java/com/greatdayhr/core/RetrofitUrl.kt
+++ core/src/main/java/com/greatdayhr/core/RetrofitUrl.kt
@@ -1,9 +1,14 @@
 package com.greatdayhr.core
 
 import me.jessyan.retrofiturlmanager.RetrofitUrlManager
+import okhttp3.OkHttpClient
 
 object RetrofitUrl {
     fun set(baseUrl: String?) {
         RetrofitUrlManager.getInstance().setGlobalDomain(baseUrl)
+    }
+
+    fun with(builder: OkHttpClient.Builder): OkHttpClient.Builder {
+        return RetrofitUrlManager.getInstance().with(builder)
     }
 }
