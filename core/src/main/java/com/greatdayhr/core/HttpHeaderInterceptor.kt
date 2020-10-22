86e7cb457f43a6fbf04a55dac980ca60862f1f84
bayu iman <senjuid@gmail.com>
2020-10-22 20:06:12 +0700

    update interceptor

--- core/src/main/java/com/greatdayhr/core/HttpHeaderInterceptor.kt
+++ core/src/main/java/com/greatdayhr/core/HttpHeaderInterceptor.kt
@@ -7,6 +7,9 @@ import okhttp3.Interceptor
 class HttpHeaderInterceptor(private val preferences: SharedPreferences) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
         var request = chain.request()
+        if (preferences.get("token") === null) {
+            return chain.proceed(request)
+        }
         request = request.newBuilder()
             .addHeader("Authorization", "${preferences.get("token")}")
             .addHeader("Content-Type", "application/json")
